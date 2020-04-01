c6d8bb402fc813f5eccad47c363e99ff119537c5
Bastian Eicher <bastian@eicher.net>
2020-04-01 20:05:18 +0200

    Replaced Travis with GitHub Actions for building and publishing documentatino

--- doc/build.ps1
+++ doc/build.ps1
@@ -14,6 +14,4 @@ mkdir ..\artifacts\Documentation | Out-Null
 0install run --batch https://apps.0install.net/devel/doxygen.xml
 if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 
-cp CNAME ..\artifacts\Documentation\
-
 popd
