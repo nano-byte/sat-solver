68998a9bc07048d98caa820f2e0f13f552821073
Bastian Eicher <bastian@eicher.net>
2020-07-11 22:12:00 +0200

    Include version number in API documentation

--- doc/build.ps1
+++ doc/build.ps1
@@ -1,8 +1,10 @@
-﻿$ErrorActionPreference = "Stop"
+﻿Param ($Version = "1.0-dev")
+$ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
-# Ensure 0install is in the PATH
+# Ensure 0install is in PATH
 if (!(Get-Command 0install -ErrorAction SilentlyContinue)) {
+    echo "Downloading 0install"
     mkdir -Force "$env:TEMP\zero-install" | Out-Null
     Invoke-WebRequest "https://0install.de/files/0install.exe" -OutFile "$env:TEMP\zero-install\0install.exe"
     $env:PATH = "$env:TEMP\zero-install;$env:PATH"
@@ -11,7 +13,7 @@ if (!(Get-Command 0install -ErrorAction SilentlyContinue)) {
 if (Test-Path ..\artifacts\Documentation) {rm -Recurse -Force ..\artifacts\Documentation}
 mkdir ..\artifacts\Documentation | Out-Null
 
-0install run --batch https://apps.0install.net/devel/doxygen.xml
+$env:VERSION = $Version; 0install run --batch https://apps.0install.net/devel/doxygen.xml
 if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 
 popd
